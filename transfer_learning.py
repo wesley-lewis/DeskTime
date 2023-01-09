9c1302466a2c7b68b84aaaba66d818184c289420
wesley-lewis <lewis.wes.23@gmail.com>
2023-01-09 18:12:08 +0530

    transfer learning code added

---
+++ transfer_learning.py
@@ -0,0 +1,3 @@
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, AveragePooling2D, Flatten, GlobalAveragePooling2D, Dense, Dropout
+from keras.layers.merge import concatenate
