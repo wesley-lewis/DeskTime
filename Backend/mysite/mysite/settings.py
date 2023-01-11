50895480005d94aabd02e2d55fa00b4a4f393ef7
wesley-lewis <lewis.wes.23@gmail.com>
2023-01-11 13:40:00 +0530

    Changed the config for the database

--- Backend/mysite/mysite/settings.py
+++ Backend/mysite/mysite/settings.py
@@ -82,10 +82,18 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
+    'default': {
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'NAME': 'desktime', # Database name
+        'USER': 'postgres',# Username 
+        'PASSWORD': 'desktime', # Password for the username
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 
