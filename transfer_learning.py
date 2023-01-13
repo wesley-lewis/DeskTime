24c61f25a443b22a10b2a337fd7c6cfd0d322f43
wesley-lewis <lewis.wes.23@gmail.com>
2023-01-13 21:18:45 +0530

    New Transfer Learning model

--- transfer_learning.py
+++
@@ -1,3 +0,0 @@
-from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, AveragePooling2D, Flatten, GlobalAveragePooling2D, Dense, Dropout
-from keras.layers.merge import concatenate
