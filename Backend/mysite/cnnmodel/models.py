e71609ae57d62dbfba9d43f98a0063c6833d68e0
wesley-lewis <lewis.wes.23@gmail.com>
2023-04-11 11:49:38 +0530

    added new attendance model

--- Backend/mysite/cnnmodel/models.py
+++ Backend/mysite/cnnmodel/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class TestAttendance(models.Model):
-  data = models.DateField()
-  amey = models.BooleanField()
-  sandesh = models.BooleanField()
-  nathan = models.BooleanField()
-  shoydon = models.BooleanField()
-  vedant = models.BooleanField()
-  manasvi = models.BooleanField()
-  srijita = models.BooleanField()
+class Attendance(models.Model):
+  # data = models.DateField()
+  # amey = models.BooleanField()
+  # sandesh = models.BooleanField()
+  # nathan = models.BooleanField()
+  # shoydon = models.BooleanField()
+  # vedant = models.BooleanField()
+  # manasvi = models.BooleanField()
+  # srijita = models.BooleanField()
+  name = models.CharField(max_length=20)
+  student_id = models.CharField(max_length=15)
+  date = models.DateTimeField()
+  class_id = models.CharField()
+  
 
